0791f7689bde008cd124e2d1a825c67e0d64b322
Main239 <accmainxyz@gmail.com>
2026-07-25 07:02:37 +0700

    v3: add getCategoryStats + moveCategory + moveAccount

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -28,6 +28,16 @@ class CategoryService {
     return maps.map((m) => Category.fromMap(m)).toList();
   }
 
+  Future<Map<String, int>> getCategoryStats({required int categoryId}) async {
+    final db = await _dbHelper.database;
+    final subResult = await db.rawQuery('SELECT COUNT(*) as cnt FROM categories WHERE parent_id = ?', [categoryId]);
+    final accResult = await db.rawQuery('SELECT COUNT(*) as cnt FROM accounts WHERE category_id = ?', [categoryId]);
+    return {
+      'subCategories': (subResult.first['cnt'] as int?) ?? 0,
+      'accounts': (accResult.first['cnt'] as int?) ?? 0,
+    };
+  }
+
   Future<List<Category>> searchCategories({required String query}) async {
     final db = await _dbHelper.database;
     final maps = await db.query('categories', where: 'name LIKE ?', whereArgs: ['%$query%'], orderBy: 'created_at DESC');
@@ -37,6 +47,11 @@ class CategoryService {
   Future<int> updateCategory({required int id, required String name}) async {
     final db = await _dbHelper.database;
     return await db.update('categories', {'name': name}, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> moveCategory({required int id, required int newParentId}) async {
+    final db = await _dbHelper.database;
+    return await db.update('categories', {'parent_id': newParentId}, where: 'id = ?', whereArgs: [id]);
   }
 
   Future<int> deleteCategory({required int id}) async {
